d851584741a461f4c3cc481dddfa78fb943ab743
Rahul Prasad <rahulrnc03@gmail.com>
2025-12-03 17:54:58 +0530

    Optimised production build

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -1,24 +1,22 @@
-import { NavLink as RouterNavLink, NavLinkProps } from "react-router-dom";
-import { forwardRef } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ComponentProps, forwardRef } from "react";
 import { cn } from "@/lib/utils";
 
-interface NavLinkCompatProps extends Omit<NavLinkProps, "className"> {
+type NavLinkProps = ComponentProps<typeof Link> & {
   className?: string;
   activeClassName?: string;
   pendingClassName?: string;
-}
+};
 
-const NavLink = forwardRef<HTMLAnchorElement, NavLinkCompatProps>(
-  ({ className, activeClassName, pendingClassName, to, ...props }, ref) => {
+const NavLink = forwardRef<HTMLAnchorElement, NavLinkProps>(
+  ({ className, activeClassName, pendingClassName: _pendingClassName, href, ...props }, ref) => {
+    const pathname = usePathname();
+    const hrefPathname = typeof href === "string" ? href : href.pathname ?? "";
+    const isActive = pathname === hrefPathname || pathname === `/${hrefPathname?.replace(/^\/+/, "")}`;
+
     return (
-      <RouterNavLink
-        ref={ref}
-        to={to}
-        className={({ isActive, isPending }) =>
-          cn(className, isActive && activeClassName, isPending && pendingClassName)
-        }
-        {...props}
-      />
+      <Link ref={ref} href={href} className={cn(className, isActive && activeClassName)} {...props} />
     );
   },
 );
